e230294dfb064feb1d83c09e71f6d4aa4ee1084b
Ji Zhang <zhangji0810@gmail.com>
2021-04-28 22:20:31 +0200

    finish Add New Expense and cancel button

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,8 +3,8 @@ import { useState } from "react";
 import "./Expenses.css";
 import Card from "../UI/Card";
 
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -13,6 +13,10 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(
+    (expense) => expense.date.getFullYear() === +filteredYear
+  );
+
   return (
     <div>
       <Card className="expenses">
@@ -20,26 +24,7 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
           selectedYear={filteredYear}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -23,11 +23,12 @@ const ExpenseForm = (props) => {
     event.preventDefault();
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
     props.onSaveExpenseData(expenseData);
+    props.setIsEditing(false);
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
@@ -66,6 +67,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
